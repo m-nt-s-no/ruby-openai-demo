bc2ad7e2f7e4f767c4154f40dd5874577630059a
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-09-19 21:37:52 +0000

    added API call from exercise

---
+++ chat.rb
@@ -0,0 +1,23 @@
+require "openai"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPEN_AI_KEY"))
+
+message_list = [
+  {
+    "role" => "system",
+    "content" => "You are a helpful assistant who talks like Shakespeare."
+  },
+  {
+    "role" => "user",
+    "content" => "Hello! What are the best spots for pizza in Chicago?"
+  }
+]
+
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp api_response
